b637b4b52df96564281ac7afd66cff7f25c085c8
Bye2-maker <godofgossiptcivs@gmail.com>
2026-02-14 14:24:21 +0800

    邏輯更改：資料夾與檔案處理

--- main.py
+++ main.py
@@ -29,9 +29,12 @@ class App:
             video.audio.write_audiofile(outP, bitrate = '192k', logger = None)
 
     def v2m_single(self):
-        f = filedialog.askopenfilename(filetypes = [('影片','*.mp4','*.mkv')])
-        s = filedialog.asksaveasfilename(defaultextension = '.mp3')
-        if not f or not s:
+        f = filedialog.askopenfilename(title = "第一步：選擇待處理檔案", filetypes = [('影片','*.mp4','*.mkv')])
+        if not f:
+            return
+
+        s = filedialog.asksaveasfilename(title = "第二步：選擇存哪", defaultextension = '.mp3', filetypes = [('*.mp3')])
+        if not s:
             return
         
         self.msg.set('等一下！！！！')
@@ -40,18 +43,23 @@ class App:
             self._V2M_(f, s)
             messagebox.showinfo('OK','去看你的檔案吧')
         except Exception as e:
-            messagebox.showerror('FAIL')
+            messagebox.showerror('FAIL失敗了')
         self.msg.set('目前無任何指令')
 
     def v2m_batch(self):
-        in_dir = filedialog.askdirectory()
-        out_dir = filedialog.askdirectory()
-        if not in_dir or not out_dir:
+        in_dir = filedialog.askdirectory(title = "第一步：選擇待處理影片資料夾")
+        if not in_dir:
+            return
+        out_dir = filedialog.askdirectory(title = "第二步：選擇存哪")
+        if not out_dir:
             return
         tar = os.path.join(out_dir, 'yourMP3')
         os.makedirs(tar, exist_ok = True)
 
         files = [f for f in os.listdir(in_dir) if f.lower().endswith(('.mp4', '.mkv'))]
+        if not files:
+            messagebox.showwarning('耍我嗎', '資料夾內沒有影片檔（.mp4/.mkv）')
+            return
         for idx, name in enumerate(files, 1):
             self.msg.set(f'目前進度{idx}/{len(files)}:{name}')
             self.root.update()
